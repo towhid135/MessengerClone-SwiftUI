2f219284531391df518ae0bc4871ca8f238f54fb
towhid135 <towhidulislam252@gmail.com>
2023-07-09 12:22:28 +0600

    project initiated

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Bayshore Communication on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
